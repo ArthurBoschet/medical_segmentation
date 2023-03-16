cd2792587d81ee223067b177a2bef6f43bb38027
Arthur Boschet <arthur.bleu25@gmail.com>
2023-03-16 15:13:57 -0400

    added single conv block

--- models/blocks/conv_blocks.py
+++ models/blocks/conv_blocks.py
@@ -156,6 +156,58 @@ class BaseConvBlock(nn.Module):
             
     def forward(self, x):
         raise NotImplementedError
+    
+
+class SingleConvBlock(BaseConvBlock):
+    def __init__(
+            self,
+            in_channels, 
+            out_channels, 
+            kernel_size, 
+            stride=1, 
+            padding=0, 
+            dilation=1,  
+            activation=nn.ReLU, 
+            normalization=nn.BatchNorm3d,
+            dropout=0
+            ):
+        super(DoubleConvBlock, self).__init__( 
+            out_channels, 
+            kernel_size, 
+            stride=stride, 
+            padding=padding, 
+            dilation=dilation,  
+            activation=activation, 
+            normalization=normalization,
+            dropout=dropout,
+        )
+        '''
+        Combines 2 basic convolutional blocks into one.
+        Parameters:
+            in_channels (int): Number of channels in the input image
+            out_channels (int): Number of channels produced by the convolution
+            kernel_size (int or tuple): Size of the convolving kernel
+            stride (int or tuple, optional): Stride of the convolution. Default: 1
+            padding (int, tuple or str, optional): Padding added to all six sides of the input. Default: 0
+            dilation (int or tuple, optional): Spacing between kernel elements. Default: 1
+            activation (def None -> torch.nn.Module): non linear activation used by the block
+            normalization (def int -> torch.nn.modules.batchnorm._NormBase): normalization
+            dropout (float): dropout added to the layer
+        '''
+            
+        self.conv_block =  self.base_block(in_channels)
+
+            
+    def forward(self, x):
+        '''
+        Parameters:
+        x (torch.Tensor): (N,C_in,D,H,W) input size
+
+        Returns:
+        x (torch.Tensor): (N,C_out,D,H,W) input size
+        '''
+        x = self.conv_block(x)
+        return x
 
     
 
