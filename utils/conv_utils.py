d3a7752996ac1b3492581eba08a66c290328cec6
Arthur Boschet <arthur.bleu25@gmail.com>
2023-03-16 12:57:11 -0400

    add calculation of output of transpose convolution to conv utils

--- utils/conv_utils.py
+++ utils/conv_utils.py
@@ -17,7 +17,26 @@ def conv_output_dim(input_dim, kernel_size, stride, padding, dilation):
   output_dim = int(np.floor((input_dim + 2*padding - dilation*(kernel_size-1) - 1)/stride + 1))
   return output_dim
 
-def conv3d_output_dim(input_dim, num_kernels, kernel_size, stride, padding, dilation):
+
+def transpose_conv_output_dim(input_dim, kernel_size, stride, padding, dilation):
+  '''
+  Calculates the output dimension of a transpose convolution operation
+  Parameters:
+    input_dim (int): spacial d imension along which convolution takes place
+    num_kernels (int): number of kernels
+    kernel_size (int): size of the kernel
+    stride (int): stride 
+    padding (int): padding
+    dilation (int): dilation 
+
+  Returns:
+    output_dim (int): new dimension at the output of the convolution operation
+  '''
+  output_dim = (input_dim-1)*stride-2*padding+dilation*(kernel_size-1)+1
+  return output_dim
+
+
+def conv3d_output_dim(input_dim, num_kernels, kernel_size, stride, padding, dilation, type='normal'):
   '''
   Calculates the output dimensions of a 3d convolution operation
   Parameters:
@@ -27,6 +46,7 @@ def conv3d_output_dim(input_dim, num_kernels, kernel_size, stride, padding, dila
     stride (Tuple[int, int, int] or int): stride along the depth, height and width dimensions respectively
     padding (Tuple[int, int, int] or int): padding along the depth, height and width dimensions respectively
     dilation (Tuple[int, int, int] or int): dilation along the depth, height and width dimensions respectively
+    type (str): either 'normal' or 'transpose'. 
 
   Returns:
     output_dim (Tuple[int, int, int , int, int]): (N, C_out, D_out, H_out, W_out)
