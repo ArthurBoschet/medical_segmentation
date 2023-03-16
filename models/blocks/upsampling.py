fb7d556c4d3f412d845d9cebe02abf2391715a1c
Arthur Boschet <arthur.bleu25@gmail.com>
2023-03-15 21:09:01 -0400

    added documentation to upsampling

--- models/blocks/upsampling.py
+++ models/blocks/upsampling.py
@@ -2,14 +2,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-#torch.nn.functional.interpolate(
+
 class InterpolateUpsample(nn.Module):
     def __init__(self, scale_factor, mode="nearest"):
+        '''
+        Upsample with interpolation
+        Parameters:
+            scale_factor (float or Tuple[float]): factor by which to up scale the tensor
+            mode (str): algorithm used for upsampling: 'nearest' | 'linear' | 'bilinear' | 'bicubic' | 'trilinear' | 'area' | 'nearest-exact'. Default: 'nearest'
+        '''
         super(InterpolateUpsample, self).__init__()
         self.mode = mode
         self.scale_factor = scale_factor
 
     def forward(self, x):
+        '''
+        Parameters:
+            x (torch.Tensor): (N, C, *) where * can be 1D,2D or 3D
+        '''
         x = F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
         return x
 
