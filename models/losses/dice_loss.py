be5d921af0101a9ec9d26fa9c78b727f61c3e4bb
Clement Detry <c.detry2507@gmail.com>
2023-03-23 12:10:07 -0400

    new eda notebook again + small changes in data loaders

--- models/losses/dice_loss.py
+++ models/losses/dice_loss.py
@@ -1,28 +1,56 @@
+import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 class DiceLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, smooth: float = 1e-7,log_loss=False):
         '''
-        Implementation of the dice loss taken from https://www.kaggle.com/code/bigironsphere/loss-function-library-keras-pytorch
-        '''
-        super(DiceLoss, self).__init__()
+        Dice loss implementation for multiclasses segmentation purposes
 
-    def forward(self, inputs, targets, smooth=1):
+        https://github.com/BloodAxe/pytorch-toolbelt/blob/develop/pytorch_toolbelt/losses/functional.py
         '''
-        Parameters:
-            inputs (torch.tensor): input from the model
-            targets (torch.tensor): target tensor
+
+        super(DiceLoss, self).__init__()
+        self.smooth = smooth
+        self.log_loss = log_loss
+
+    def forward(self,pred,target):
         '''
-        
-        #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)       
-        
-        #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-        
-        intersection = (inputs * targets).sum()                            
-        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
-        
-        return 1 - dice
+
+        Parameters
+        ----------
+        pred (torch.Tensor) : tensors which contains prediction (N,NC,D,H,W)
+        target (torch.Tensor): tensor which contains actual labels (N,D,H,W)
+        N = batch size , NC = number of classes, D= depth , H = height, W = width
+
+        Returns
+
+        dice score loss (float): loss value for dice score
+        -------
+        '''
+
+        # get dimensions of samples
+        batch_size = pred.shape[0]
+        n_class = pred.shape[1]
+
+        # we get one hot encoding of target prediction
+        target = nn.functional.one_hot(target, n_class)
+        # we permute to get same shape as prediction tensor
+        target = torch.permute(target, (0,4,1,2,3))
+
+        # check if pred size and target size are a match
+        assert pred.shape == target.shape
+
+        # logits to probability for prediction
+        pred = nn.functional.softmax(pred,dim=1)
+
+        # we switch to flatten version of vectors
+        pred = pred.view(batch_size, n_class, -1)
+        target = target.view(batch_size,n_class,-1)
+
+        # calculate multi-class dice loss
+        inter = torch.sum(pred*target)
+        card  = torch.sum(pred+target)
+        dice_score = 2*(inter+self.smooth)/(card+self.smooth)
+        loss =  1 - dice_score if not self.log_loss else -torch.log(dice_score)
+        return loss
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -60,40 +60,6 @@ def convert_to_numpy(input_folder):
     else:
         raise ValueError('Images format not recognized (should be .npz or .nii.gz)')
 
-def get_resize_shape(input_folder, factor=2):
-    '''
-    Get the shape to resize the images and labels from the dataset to
-    
-    Args:
-        input_folder: str 
-            Path to the folder containing the images and labels
-        factor: int
-            Factor to resize the images and labels by
-    '''
-    width_mean = 0
-    height_mean = 0
-    depth_mean = 0
-    for subdir in ["train", "val"]:
-        dir = os.path.join(input_folder, subdir)
-        if os.path.isdir(dir):
-            for file in tqdm(os.listdir(dir)):
-                if file.endswith('.npy'):
-                    if file.startswith('image'):
-                        data = np.load(os.path.join(dir, file))
-                        depth_mean += data.shape[0]
-                        height_mean += data.shape[1]
-                        width_mean += data.shape[2]
-    depth_mean /= (len(os.listdir(os.path.join(input_folder, "train"))) + len(os.listdir(os.path.join(input_folder, "val"))))//2
-    height_mean /= (len(os.listdir(os.path.join(input_folder, "train"))) + len(os.listdir(os.path.join(input_folder, "val"))))//2
-    width_mean /= (len(os.listdir(os.path.join(input_folder, "train"))) + len(os.listdir(os.path.join(input_folder, "val"))))//2
-    depth_mean /= factor
-    height_mean /= factor
-    width_mean /= factor
-    depth_mean = int(np.ceil(depth_mean/factor) * factor)
-    height_mean = int(np.ceil(height_mean/factor) * factor)
-    width_mean = int(np.ceil(width_mean/factor) * factor)
-    return (depth_mean, height_mean, width_mean)
-
 def prepare_dataset_for_training(dataset_folder_path, output_dataset_path, val_size=0.2):
     '''
     Prepare the dataset for training by splitting it into training and validation sets for the dataloaders
@@ -120,6 +86,7 @@ def prepare_dataset_for_training(dataset_folder_path, output_dataset_path, val_s
     # create train and val folders
     os.makedirs(os.path.join(output_dataset_path, "train"))
     os.makedirs(os.path.join(output_dataset_path, "val"))
+    os.makedirs(os.path.join(output_dataset_path, "test"))
 
     # copy validation images and labels to val folder
     for val_im_file, val_label_file in zip(val_images_files, val_labels_files):
@@ -129,6 +96,9 @@ def prepare_dataset_for_training(dataset_folder_path, output_dataset_path, val_s
     for tr_im_file, tr_label_file in zip(train_images_files, train_labels_files):
         shutil.copyfile(os.path.join(dataset_folder_path, "imagesTr", tr_im_file), os.path.join(output_dataset_path, "train", tr_im_file))
         shutil.copyfile(os.path.join(dataset_folder_path, "labelsTr", tr_label_file), os.path.join(output_dataset_path, "train", tr_label_file))
+    # copy test images to test folder
+    for test_im_file in os.listdir(os.path.join(dataset_folder_path, "imagesTs")):
+        shutil.copyfile(os.path.join(dataset_folder_path, "imagesTs", test_im_file), os.path.join(output_dataset_path, "test", test_im_file))
 
     # renames train files
     for i, (im_tr, la_tr) in enumerate(zip(sorted(train_images_files), sorted(train_labels_files))):
@@ -138,7 +108,11 @@ def prepare_dataset_for_training(dataset_folder_path, output_dataset_path, val_s
     for i, (im_val, la_val) in enumerate(zip(sorted(val_images_files), sorted(val_labels_files))):
         os.rename(os.path.join(output_dataset_path, "val", im_val), os.path.join(output_dataset_path, "val", f"image_{str(i).zfill(3)}.npy"))
         os.rename(os.path.join(output_dataset_path, "val", la_val), os.path.join(output_dataset_path, "val", f"label_{str(i).zfill(3)}.npy"))
+    # renames test files
+    for i, test_im in enumerate(sorted(os.listdir(os.path.join(output_dataset_path, "test")))):
+        os.rename(os.path.join(output_dataset_path, "test", test_im), os.path.join(output_dataset_path, "test", f"image_{str(i).zfill(3)}.npy"))
 
     # remove useless folders
     shutil.rmtree(os.path.join(output_dataset_path, "imagesTr"))
     shutil.rmtree(os.path.join(output_dataset_path, "labelsTr"))
+    shutil.rmtree(os.path.join(output_dataset_path, "imagesTs"))
