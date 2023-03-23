6266d85924c3bcda10bd17032b96efe0ed1faf47
Clement Detry <c.detry2507@gmail.com>
2023-03-23 15:06:08 -0400

    iou score update

--- models/evaluation/metrics.py
+++ models/evaluation/metrics.py
@@ -1,20 +1,23 @@
+def iou_score(preds, targets, eps=1e-7):
+    """
+    Compute the Intersection over Union (IoU)
 
-def iou_score(pred_mask, target_mask):
-    assert pred_mask.shape == target_mask.shape, "Shape mismatch in input masks."
+    Args:
+        preds (torch.Tensor): 
+            Predictions probabilities from the model (B, C, D, H, W)
+        targets (torch.Tensor):
+            Ground truth labels (B, C, D, H, W)
+        eps (float):
+            Small value to avoid division by zero
+    """
+    # Flatten the predictions and targets
+    preds = preds.view(-1)
+    targets = targets.view(-1)
 
-    # Make sure the input tensors are boolean
-    pred_mask = pred_mask.bool()
-    target_mask = target_mask.bool()
+    # Compute the intersection and union
+    intersection = (preds * targets).sum()
+    union = preds.sum() + targets.sum() - intersection
 
-    # Calculate intersection
-    intersection = (pred_mask & target_mask).float().sum()
-
-    # Calculate union
-    union = (pred_mask | target_mask).float().sum()
-
-    # Calculate IoU
-    iou = intersection / (union + 1e-6)  # Adding a small epsilon to avoid division by zero
-
-    return iou
-
-
+    # Compute the IoU score and return the average across all classes and batches
+    iou = (intersection + eps) / (union + eps)
+    return iou.mean().item()
